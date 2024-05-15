888e5461b98d11994d0c3d6603b8c6b33f492750
adeebbayat <adeebbayat@yahoo.com>
2024-05-15 13:40:48 -0700

    attempted fix for deployment not working

--- backend/server.js
+++ backend/server.js
@@ -1,43 +1,56 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 const userController = require('./controllers/userController');
 const skillController = require('./controllers/skillController');
 const app = express();
-const port = 5002;
+const port = process.env.PORT || 5002;
 
-const mongoURI = 'mongodb+srv://username:eEsYcktqhWGNJYFj@cluster0.hh5fdrv.mongodb.net/takeHome?retryWrites=true&w=majority&appName=Cluster0'
-mongoose.connect(mongoURI);
+const mongoURI = 'mongodb+srv://username:eEsYcktqhWGNJYFj@cluster0.hh5fdrv.mongodb.net/takeHome?retryWrites=true&w=majority&appName=Cluster0';
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-app.use(cors()); 
+// Middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Skills
-  app.get('/skills', skillController.getAllSkills, (req, res) => {
-    res.send(res.locals.skills);
-  })
+app.get('/skills', skillController.getAllSkills, (req, res) => {
+  res.send(res.locals.skills);
+});
 
-  app.post('/skills', skillController.createSkill, (req, res) => {
-    res.send('Succesfully Created')
-  })
+app.post('/skills', skillController.createSkill, (req, res) => {
+  res.send('Successfully Created');
+});
 
 // Users
-  app.get('/users', userController.getAllUsers, (req, res) => {
-    res.send(res.locals.users);
-  })
+app.get('/users', userController.getAllUsers, (req, res) => {
+  res.send(res.locals.users);
+});
 
-  app.get(`/users/*`, userController.getOneUser, (req, res) => {
-    res.send(res.locals.user);
-  })
+app.get('/users/*', userController.getOneUser, (req, res) => {
+  res.send(res.locals.user);
+});
 
-  app.post('/users', userController.createUser, (req, res) => {
-    res.send('Succesfully Created')
-  })
+app.post('/users', userController.createUser, (req, res) => {
+  res.send('Successfully Created');
+});
 
-  app.put(`/users/*`, userController.updateUser, (req, res) => {
-    res.send('Updated')
-  })
+app.put('/users/*', userController.updateUser, (req, res) => {
+  res.send('Updated');
+});
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '..', 'frontend', 'build')));
+
+// The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'frontend', 'build', 'index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
